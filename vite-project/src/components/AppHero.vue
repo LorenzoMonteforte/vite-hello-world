33655b7d60caa622e2cd170ffdb2f87b78b496e4
Lorenzo Monteforte <lorenzomonteforte.mm@mail.com>
2023-11-28 00:31:03 +0100

    Bonus

---
+++ vite-project/src/components/AppHero.vue
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<template>
+    <img src="https://i.pinimg.com/originals/09/6a/35/096a35453660aa9b83ba4ab6d9182d61.jpg" alt="">
+</template>
+
+<style scoped>
+img {
+    width: 100%;
+}
+</style>
