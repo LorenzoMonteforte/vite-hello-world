8696ac2ab11bb685e523a2de2692c5fdbb784471
Lorenzo Monteforte <lorenzomonteforte.mm@mail.com>
2023-11-28 01:18:59 +0100

    Mio bonus (2) (Fontawesome)

--- vite-project/src/components/Principale.vue
+++ vite-project/src/components/Principale.vue
@@ -3,12 +3,20 @@
 </script>
 
 <template>
-    <main></main>
+    <main>
+        <font-awesome-icon icon="fa-solid fa-check" />
+        <font-awesome-icon icon="fa-solid fa-x" />
+    </main>
 </template>
 
 <style scoped>
 main {
     height: 1000px;
     background-color: red;
+}
+
+svg {
+    color: white;
+    font-size: 3rem;
 }
 </style>
--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -1,5 +1,22 @@
+/* Set up using Vue 3 */
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faCheck } from '@fortawesome/free-solid-svg-icons'
+import { faX } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faCheck)
+library.add(faX)
+
+createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app')
